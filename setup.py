626ce28fbc5c4d1869d905ccfe853f36dc48a8ec
Mironov Daniil <Hugant@users.noreply.github.com>
2025-07-25 15:34:12 +0700

    Bump version to 1.1.14 in setup.py
    Updated the package version from 1.1.13 to 1.1.14 to prepare for a new release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='bojango',
-  version='1.1.13',
+  version='1.1.14',
   description='A lightweight framework for creating Telegram bots with advanced routing and localization.',
   long_description=open('README.md', encoding='utf-8').read(),
   long_description_content_type='text/markdown',
